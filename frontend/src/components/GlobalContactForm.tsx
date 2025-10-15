ebd5e65b3e1fa242b53ba4a6804ba6ce8b7c30ef
Bhaskar <bassnaidu1242@gmail.com>
2025-10-15 19:54:03 +0530

    fixed contact form

--- frontend/src/components/GlobalContactForm.tsx
+++ frontend/src/components/GlobalContactForm.tsx
@@ -25,7 +25,7 @@ const GlobalContactForm = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch("http://localhost:8000/send-mail", {
+      const response = await fetch("https://rass-2.onrender.com/send-mail", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
